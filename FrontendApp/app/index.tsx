2c534402973c63bf3583abdcc7e7ccb86ebb3e25
Eryoneta <eryoneta@gmail.com>
2026-03-29 19:43:00 -0300

    [UPD] FrontendApp: Conexão com backend
    - (UPD) Implementado conexão com backend
    - (UPD) Implementado status

--- FrontendApp/app/index.tsx
+++ FrontendApp/app/index.tsx
@@ -1,6 +1,27 @@
+import React from "react";
 import { Text, View } from "react-native";
+import { CurrentStatus } from "./status";
 
 export default function Index() {
+  const status = CurrentStatus;
+
+  const [ backendInfo, setBackendInfo ] = React.useState(null);
+
+  const apiEndpoint = "http://localhost:3000/api";
+  const getInfo = async() => {
+    await fetch(`${apiEndpoint}/info`)
+      .then(data => data.json())
+      .then(json => {
+          console.log("Info: OK");
+          setBackendInfo(json);
+      })
+      .catch((error) => {
+          console.error("Info: ERROR");
+          console.error(error);
+      });
+  };
+  getInfo();
+
   return (
     <View
       style={{
@@ -9,7 +30,9 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text>App version: {status.CURRENT_VERSION}</Text>
+      <Text>Backend version: {backendInfo?.version}</Text>
     </View>
   );
 }
+
---
+++ FrontendApp/app/status.ts
@@ -0,0 +1,7 @@
+export class CurrentStatus {
+    public static GROUP_NAME = "Univespers";
+    public static PROJECT_NAME = "PM-App";
+    public static CURRENT_VERSION = "app-1.0.0-alpha";
+
+    constructor() {}
+}
