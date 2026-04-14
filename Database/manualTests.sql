e68956dc5abb3a2ffa70af1690e080c82409d313
Eryoneta <eryoneta@gmail.com>
2026-04-13 23:44:58 -0300

    [UPD] Database/Tests: Testes no Admin
    - (UPD) Implementado testes das procedures do Admin

--- Database/manualTests.sql
+++ Database/manualTests.sql
@@ -10,3 +10,12 @@ SELECT * FROM Admin; -- Deve conter o novo admin
 -- Login de Admin
 CALL LOGIN_ADMIN('000.000.000-00', 'senha123');
 SELECT * FROM Login; -- Deve conter o novo admin
+
+-- Logout de Admin
+CALL LOGOUT_ADMIN(''); -- Requer o LoginUUID!
+SELECT * FROM Login; -- Deve estar vazio
+
+-- Remoção de Admin
+CALL LOGIN_ADMIN('000.000.000-00', 'senha123');
+CALL DELETE_ADMIN(''); -- Requer o LoginUUID!
+SELECT * FROM Admin; -- Deve estar vazio
