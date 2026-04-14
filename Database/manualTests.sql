94ad7cf961b398130c7c6d53d540b7b14a0a0982
Eryoneta <eryoneta@gmail.com>
2026-04-14 18:11:56 -0300

    [UPD] Database/Testes: Testes de User
    - (ADD) Testes manuais relacionados ao User

--- Database/manualTests.sql
+++ Database/manualTests.sql
@@ -3,13 +3,16 @@
 -- Usar o banco de dados
 USE pm_manager;
 
+
+-- Admin
+
 -- Criação de Admin
 CALL CREATE_ADMIN('000.000.000-00', 'senha123');
 SELECT * FROM Admin; -- Deve conter o novo admin
 
 -- Login de Admin
 CALL LOGIN_ADMIN('000.000.000-00', 'senha123');
-SELECT * FROM Login; -- Deve conter o novo admin
+SELECT BIN_TO_UUID(uuid) FROM Login; -- Deve conter o novo admin
 
 -- Logout de Admin
 CALL LOGOUT_ADMIN(''); -- Requer o LoginUUID!
@@ -19,3 +22,24 @@ SELECT * FROM Login; -- Deve estar vazio
 CALL LOGIN_ADMIN('000.000.000-00', 'senha123');
 CALL DELETE_ADMIN(''); -- Requer o LoginUUID!
 SELECT * FROM Admin; -- Deve estar vazio
+
+
+-- User
+
+-- Criação de User
+CALL CREATE_ADMIN('000.000.000-00', 'senha123');
+CALL LOGIN_ADMIN('000.000.000-00', 'senha123');
+CALL CREATE_USER('000.000.000-00', 'senha123',''); -- Requer o LoginUUID do Admin!
+SELECT * FROM User; -- Deve conter o novo user
+
+-- Login de User
+CALL LOGIN_USER('000.000.000-00', 'senha123');
+SELECT BIN_TO_UUID(uuid) FROM Login WHERE user_id IS NOT NULL; -- Deve conter o novo user
+
+-- Logout de User
+CALL LOGOUT_USER(''); -- Requer o LoginUUID do User!
+SELECT BIN_TO_UUID(uuid) FROM Login WHERE user_id IS NOT NULL; -- Deve estar vazio
+
+-- Remoção de User
+CALL DELETE_USER('000.000.000-00', ''); -- Requer o LoginUUID do Admin!
+SELECT * FROM User; -- Deve estar vazio
