e96f713ecd76adadc6ca8404f2bf65859481f7c0
Eryoneta <eryoneta@gmail.com>
2026-04-13 22:01:16 -0300

    [ADD] Database: Testes
    - (ADD) Script para testes manuais das procedures
    . Deve ser executado linha-a-linha manualmente!

---
+++ Database/manualTests.sql
@@ -0,0 +1,12 @@
+-- Testes manuais
+
+-- Usar o banco de dados
+USE pm_manager;
+
+-- Criação de Admin
+CALL CREATE_ADMIN('000.000.000-00', 'senha123');
+SELECT * FROM Admin; -- Deve conter o novo admin
+
+-- Login de Admin
+CALL LOGIN_ADMIN('000.000.000-00', 'senha123');
+SELECT * FROM Login; -- Deve conter o novo admin
